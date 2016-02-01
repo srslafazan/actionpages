3d32420634b2ad80308af377d1efa53bde158606
Alexander Kehaya <afkehaya@gmail.com>
2016-01-31 20:07:23 -0800

    added modal to refferal page and tweaked views

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,6 +3,4 @@ class Page < ActiveRecord::Base
 	has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "1000X1000" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
-	has_a :referral
-
 end
