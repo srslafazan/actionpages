02ef445fcb16ace5ee6f18bc680f035107e1e3d6
Alexander Kehaya <afkehaya@gmail.com>
2016-02-01 21:52:22 -0800

    reworked referral view

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -1,6 +1,6 @@
 class Referral < ActiveRecord::Base
 
-	has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "1000X1000" }
+	has_attached_file :image, styles: { small: "64x64", med: "500x500", large: "1000X1000" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
 	belongs_to :page
